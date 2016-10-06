cd7d5647c36f51666e59da60ee5d27a212456482
Shih-Wei Li <shihwei@cs.columbia.edu>
2016-10-06 20:06:21 +0000

    arm64: EL2: KVM: Patch out hyp_va address conversions
    Just like with VHE, we don't need to translate any addresses in EL2 when
    running the kernel in EL2 on ARMv8.  Do this at compile-time for now.
    
    Signed-off-by: Christoffer Dall <cdall@cs.columbia.edu>
    Signed-off-by: Shih-Wei Li <shihwei@cs.columbia.edu>

--- arch/arm64/include/asm/kvm_mmu.h
+++ arch/arm64/include/asm/kvm_mmu.h
@@ -117,6 +117,7 @@ alternative_endif
 #include <asm/mmu_context.h>
 #include <asm/pgtable.h>
 
+#ifndef CONFIG_EL2_KERNEL
 static inline unsigned long __kern_hyp_va(unsigned long v)
 {
 	asm volatile(ALTERNATIVE("and %0, %0, %1",
@@ -133,6 +134,9 @@ static inline unsigned long __kern_hyp_va(unsigned long v)
 }
 
 #define kern_hyp_va(v) 	(typeof(v))(__kern_hyp_va((unsigned long)(v)))
+#else /* CONFIG_EL2_KERNEL */
+#define kern_hyp_va(v) (v)
+#endif
 
 /*
  * We currently only support a 40bit IPA.
